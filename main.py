1ff8f1b93b94b13e591a4b0ac2f7771c2e34d3ef
p-r-o-m-e <97275566+p-r-o-m-e@users.noreply.github.com>
2022-01-25 23:08:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import discord
 from dotenv import load_dotenv
+# import sys
 
 load_dotenv()
+
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 
@@ -12,9 +14,15 @@ bot = discord.Client()
 async def on_ready():
     for guild in bot.guilds:
       guild_count = 0
-      print(f"- {guild.id} (name: {guild.name})")
+      print(f"(id: {guild.id}) (name: {guild.name})")
       guild_count = guild_count + 1
     print("DiscordBot is in " + str(guild_count) + " guilds.")
+@bot.event
+async def on_message(message):
+    #print(f"checking")
+    if bot.user.mentioned_in(message):
+        await message.channel.send('You mentioned me!')
 
 
 bot.run(TOKEN)
+
